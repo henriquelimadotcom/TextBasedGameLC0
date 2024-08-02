eaa09e2379a38bbde24f3702d255a343aae48541
lowc0ff <lowc0ff@lowc0ff>
2024-08-02 07:20:17 +0200

    version 1.0

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,25 +3,30 @@
 
 Game::Game() {
     playerPosition = "Start";
-    locations["Start"] = "You are at the start of the forest.";
-    locations["Clearing"] = "You are in a clearing with sunlight filtering through the trees.";
-    locations["River"] = "You hear the sound of water. You are at the river bank.";
-    locations["Cave"] = "It's dark inside, but you can see a faint glow. You are in a cave.";
-    locations["Treasure"] = "Congratulations! You have found the hidden treasure!";
-    paths["Start"]["North"] = "Clearing";
-    paths["Start"]["East"] = "River";
-    paths["Clearing"]["South"] = "Start";
-    paths["Clearing"]["East"] = "Cave";
-    paths["River"]["West"] = "Start";
-    paths["Cave"]["West"] = "Clearing";
-    paths["Cave"]["East"] = "Treasure";
+    locations["Start"] = "****You are at the start of the forest.";
+    locations["Clearing"] = "****You are in a clearing with sunlight filtering through the trees.";
+    locations["River"] = "****You hear the sound of water. You are at the river bank.";
+    locations["Cave"] = "****It's dark inside, but you can see a faint glow. You are in a cave.";
+    locations["Treasure"] = "$$$$Congratulations! You have found the hidden treasure!";
+    paths["Start"]["north"] = "Clearing";
+    paths["Start"]["east"] = "River";
+    paths["Clearing"]["south"] = "Start";
+    paths["Clearing"]["east"] = "Cave";
+    paths["River"]["west"] = "Start";
+    paths["Cave"]["west"] = "Clearing";
+    paths["Cave"]["east"] = "Treasure";
 }
 
 void Game::start() {
 	while (true) {
 		displayLocation();
 		std::string direction;
-		std::cout << "Enter direction (North, South, East, West) or 'quit' to exit: ";
+
+			if (playerPosition == "Start") std::cout << "Enter direction (north, east) or 'quit' to exit: ";
+			if (playerPosition == "Clearing") std::cout << "Enter direction (south, east) or 'quit' to exit: ";
+			if (playerPosition == "River") std::cout << "Enter direction (west) or 'quit' to exit: ";
+			if (playerPosition == "Cave") std::cout << "Enter direction (west, east) or 'quit' to exit: ";
+
 		std::cin >> direction;
 		if (direction == "quit") break;
 		move(direction);
